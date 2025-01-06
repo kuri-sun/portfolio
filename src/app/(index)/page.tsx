30930d7cadec3283d4d58b1586ef3d6d9a16b540
HaRuki Kuriwada <soccer_haruki15@me.com>
2025-01-06 17:20:09 +0900

    updated for seo improvement

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -2,11 +2,15 @@ import { Hero } from "@/components/hero/Hero";
 import { About } from "@/components/about/About";
 import Projects from "@/components/projects/Projects";
 import { Header } from "@/components/header/Header";
-import { Logs } from "@/components/logs/Logs";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Haruki Kuriwada portfolio</title>
+        <link rel="canonical" href="https://rukikuri.com/" key="canonical" />
+      </Head>
       <Header />
       <div
         className={`flex flex-col pb-24 dark:bg-neutral-800 text-gray-600 dark:text-neutral-100`}
